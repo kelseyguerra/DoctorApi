9043eb5a2bf14082447f0e739eb9a97f93c3cbfb
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-04-17 21:36:12 -0700

    add bable

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,10 @@ module.exports = {
           /node_modules/,
           /spec/
         ],
-        loader: "eslint-loader"
+        loader: "babel-loader",
+        options: {
+          presets: ['es2015']
+        }
       }
     ]
   }
