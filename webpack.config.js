70479bffb2b15c146d7e643d735d36001771dfbb
Guest User <Guest@Epicodus-9C.local>
2018-04-26 15:12:44 -0700

    create template

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -16,7 +17,8 @@ module.exports = {
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({,
+    new Dotenv()
       title: 'Ping Pong',
       template: './src/index.html',
       inject: 'body'
