ef442917044b3296e2f491335ff190a984e56959
kelsey guerra <kelseylguerra@gmail.com>
2018-04-27 10:50:08 -0700

    add form & user input to index

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-// import {  } from './scripts';
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
+import { ApiCall } from './doctor-api.js';
+import { getDoctor } from './doctor.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+// function clearData(){
+//   $('#symptom').val('');
+//   $('#name').val('');
+//   $('#output').empty();
+// }
+//
+// function getResults(response) {
+//   let body = JSON.parse(response);
+//   if (body.doctor === null) {
+//     $('#output').text("Doctor not found. Please try again.");
+//   } else {
+//     $('#symptom').append(body.doctor)
+//
+//   }
+//   }
+// }
